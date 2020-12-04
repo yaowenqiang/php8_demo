927b74b445e5eeed1907502677eb7ebd8b8fc541
Yaowenqiang <yaowenqiang111@163.com>
2020-12-04 22:36:11 +0800

    add matches demo

--- features.php
+++ features.php
@@ -1,5 +1,7 @@
 <?php
 var_dump(htmlspecialchars('!@#$%',ENT_COMPAT|ENT_HTML401));
 var_dump(htmlspecialchars('!@#$%',double_encode: false));
-
-
+echo match(8.0) {
+    "8.0" => "hello",
+    8.0 => "888",
+};
